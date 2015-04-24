cfe8125bc860a00e666a2473935ce26a8225dde1
derekh <derek.hampton@augmensis.co.uk>
2015-04-24 13:46:28 +0100

    Added Keeping Track Of Criminals solution

--- BankTech-Python/2-KeepingTrackOfCriminals/_2_KeepingTrackOfCriminals.py
+++ BankTech-Python/2-KeepingTrackOfCriminals/_2_KeepingTrackOfCriminals.py
@@ -1,1 +1,16 @@
-print('Hello World')
+serialNumber = ""
+while serialNumber == "":
+    serialNumber = input("Please input the serial number to print:")
+
+timesToPrint = 0
+while timesToPrint == 0:
+    someNumber = input("How many times should we print %s?" % serialNumber)
+    if someNumber.isdigit():
+        timesToPrint = int(someNumber)
+    else:
+        print("Please enter a valid number!")
+
+for i in range(0, timesToPrint):
+    print(serialNumber)
+
+print("\n%s was printed %d times" % (serialNumber, timesToPrint))
