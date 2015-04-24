75522bfc11eac52a41090a677f95ff7eb4ef81ca
derekh <derek.hampton@augmensis.co.uk>
2015-04-24 13:25:52 +0100

    Added Call Centre Spam solution

--- BankTech-Python/1-CallCentreSpam/_1_CallCentreSpam.py
+++ BankTech-Python/1-CallCentreSpam/_1_CallCentreSpam.py
@@ -1,1 +1,14 @@
-print('Hello World')
+phoneList = open("PhoneList.txt", "r")
+
+goodCount = 0
+spamCount = 0
+
+for phoneNumber in phoneList:
+    if "SPAM" in phoneNumber:
+        print("SPAM")
+        spamCount += 1
+    else:
+        print("OK")
+        goodCount += 1
+
+print("%s numbers processed.\n%s SPAM numbers found.\n%s numbers were OK" % (goodCount + spamCount, spamCount, goodCount))
